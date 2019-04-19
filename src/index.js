8396bef6213584e18799c7872cd78bd9551aee3a
Student <example@example.com>
2019-04-18 17:55:20 -0700

    add about me page and inactive sub menu items in Web Dev dropdown

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ $( document ).ready(function() {
   $(".dev").click(function() {
     $('.transform').toggleClass('transform-active');
     $('.dev').toggleClass('dev-active');
-    
-      
-    if($('body').hasClass('body-active')){      
+
+
+    if($('body').hasClass('body-active')){
       $( 'body' ).removeClass( "body-trans" ).addClass( "body-active" );
     }
     $('.body-trans').toggleClass('body-active');
-    
+
     if($('.proficiencies').is(':visible')){
        $('.proficiencies').fadeOut(1000);
     }
@@ -19,41 +19,54 @@ $( document ).ready(function() {
     }
     console.log("hello")
   });
-  
-  
-  
-  
-  
+
+
+
+
+
   $(".prod").click(function() {
-    
-      
-    
-    
+
+
+
+
     $('.transform-prod').toggleClass('transform-active-prod');
     $('.prod').toggleClass('prod-active');
-    
-    if($('body').hasClass('body-active')){      
+
+    if($('body').hasClass('body-active')){
       $( 'body' ).removeClass( "body-trans" ).addClass( "body-active" );
-    }    
+    }
     $('.body-trans').toggleClass('body-active');
-    
+
     console.log("hello")
   });
-  
-  
-  
-  
-  
+
+
+
+
+
   $(".vid").click(function() {
-    
-    
+
+
     $('.transform-vid').toggleClass('transform-active-vid');
     $('.vid').toggleClass('vid-active');
-    if($('body').hasClass('body-active')){      
+    if($('body').hasClass('body-active')){
       $( 'body' ).removeClass( "body-trans" ).addClass( "body-active" );
-    }    
+    }
     $('.body-trans').toggleClass('body-active');
-    
+
     console.log("hello")
   });
+});
+
+$(".bio").click(function() {
+
+
+  $('.transform-bio').toggleClass('transform-active-bio');
+  $('.biog').toggleClass('bio-active');
+  if($('body').hasClass('body-active')){
+    $( 'body' ).removeClass( "body-trans" ).addClass( "body-active" );
+  }
+  $('.body-trans').toggleClass('body-active');
+
+  console.log("hello")
 });
