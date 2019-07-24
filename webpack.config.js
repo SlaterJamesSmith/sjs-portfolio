206d02f527b6c7caa7a44608bd2532a2c3e72981
Slater Smith <smithslater@gmail.com>
2019-07-23 21:01:23 -0700

    add pdf rule to webpad config file loader - it doesn't work currently. Add new links to make all buttons but pdf download funtional

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
        ]
      },
      {
-       test: /\.(gif|png|jpe?g)$/,
+       test: /\.(gif|png|jpe?g|pdf)$/,
        use: [
          {
            loader: 'file-loader',
